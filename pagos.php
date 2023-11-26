a1479329e13e89f599d8f5afd9dbac7620573ded
Abrotello <adrianbotellog01@gmail.com>
2023-11-25 19:43:57 -0600

    Segunda parte del proyecto

---
+++ pagos.php
@@ -0,0 +1,46 @@
+<?php
+    $connection = mysqli_connect('localhost', 'root', 'root', 'smart_boxing');
+
+    if(!$connection) {
+        echo 'Error de conexion a la BD...'. mysqli_connect_error();
+        exit();
+    }
+    else{
+        session_start();
+
+        $articulo = $_POST['producto'];
+        $cliente = $_SESSION["id_person"];
+        $rol = $_SESSION["rol"];
+
+        if(!isset($cliente)){
+            header('Location: sesion.html');
+        }else{
+            $resultado = mysqli_query ($connection, "SELECT * FROM inventory WHERE ID_item = '$articulo'");
+    
+            if (!$resultado)
+            {
+                echo 'Error en la Consulta.'.mysqli_connect_error();
+                header('Location: productos.php');
+            }
+            else{
+                $rowcount=mysqli_num_rows($resultado);
+                if($rowcount == 0){
+                    header('Location: productos.php');
+                }else {
+                    $row = mysqli_fetch_array($resultado);
+                    $stock = $row["stock"] - '1';
+
+                    $resultado = mysqli_query($connection, "UPDATE inventory SET stock = '$stock' WHERE ID_item = '$articulo'");
+
+                    $total_venta = $row["precio"];
+                    $fecha = date_create('now')->format('Y-m-d H:i:s');
+                    $cantidad = '1';
+
+                    $resultado = mysqli_query($connection, "INSERT INTO sales(articulo, cliente, total_venta, fecha, cantidad) VALUES( '$articulo', '$cliente', '$total_venta',  '$fecha', '$cantidad')");
+
+                    header('Location: tarjeta.php');
+                }
+            }   
+        }
+    }
+?>
